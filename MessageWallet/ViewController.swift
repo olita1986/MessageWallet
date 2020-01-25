baf23879200cf492527e7583f276c307d77fe2a3
Orlando Arzola Aragort <orlandoarzola86@me.com>
2020-01-25 09:36:26 -0300

    Initial Commit

---
+++ MessageWallet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MessageWallet
+//
+//  Created by Orlando Arzola Aragort on 1/25/20.
+//  Copyright © 2020 Orlando Arzola Aragort. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
